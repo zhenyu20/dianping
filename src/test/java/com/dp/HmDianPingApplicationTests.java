ee7e1b78ab044f4da79d99436d3f26febaaadfd1
zhenyu20 <azy20804@gmail>
2023-12-07 21:41:34 +0800

    Util:CacheClient

--- src/test/java/com/dp/HmDianPingApplicationTests.java
+++ src/test/java/com/dp/HmDianPingApplicationTests.java
@@ -1,20 +1,28 @@
 package com.dp;
 
+import com.dp.entity.Shop;
 import com.dp.service.impl.ShopServiceImpl;
+import com.dp.utils.CacheClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
+
 
 @SpringBootTest
 public class HmDianPingApplicationTests {
+
+    @Resource
+    private CacheClient cacheClient;
 
     @Resource
     private ShopServiceImpl shopService;
 
     @Test
     public void test(){
-        shopService.saveShop2Redis(1L,2000);
+        Shop shop = shopService.getById(1);
+        cacheClient.setWithLogicalExpire("cache:shop:" + 1,shop,1L, TimeUnit.SECONDS);
     }
 
 }
