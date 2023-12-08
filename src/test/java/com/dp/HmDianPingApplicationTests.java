0ddb56ff2745ee3e7a662ca97ab7d231699afbab
zhenyu20 <azy20804@gmail>
2023-12-08 13:09:24 +0800

    incID

--- src/test/java/com/dp/HmDianPingApplicationTests.java
+++ src/test/java/com/dp/HmDianPingApplicationTests.java
@@ -3,10 +3,14 @@ package com.dp;
 import com.dp.entity.Shop;
 import com.dp.service.impl.ShopServiceImpl;
 import com.dp.utils.CacheClient;
+import com.dp.utils.RedisIdWorker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 
@@ -19,10 +23,36 @@ public class HmDianPingApplicationTests {
     @Resource
     private ShopServiceImpl shopService;
 
+    @Resource
+    private RedisIdWorker redisIdWorker;
+
+    private ExecutorService es = Executors.newFixedThreadPool(500);
+
     @Test
     public void test(){
         Shop shop = shopService.getById(1);
         cacheClient.setWithLogicalExpire("cache:shop:" + 1,shop,1L, TimeUnit.SECONDS);
+    }
+
+    //测试全局ID
+    @Test
+    void testIdWorker() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(300);
+
+        Runnable task = () -> {
+            for (int i = 0; i < 100; i++) {
+                long id = redisIdWorker.nextId("order");
+                System.out.println("id = " + id);
+            }
+            latch.countDown();
+        };
+        long begin = System.currentTimeMillis();
+        for (int i = 0; i < 300; i++) {
+            es.submit(task);
+        }
+        latch.await();
+        long end = System.currentTimeMillis();
+        System.out.println("time = " + (end - begin));
     }
 
 }
